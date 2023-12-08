f350ce116c2f7034128e29799b17886a5f7b9a4e
Muhammad Hamza <150414868+Hamza123545@users.noreply.github.com>
2023-12-08 10:48:15 +0500

    Add files via upload
    fdsa

---
+++ Advance js session 2/calculator.js
@@ -0,0 +1,24 @@
+ // calculator with functions of javascript
+ 
+ var btn1 = document.getElementById("cal1")
+ var btn2 = document.getElementById("cal2")
+ var btn3 = document.getElementById("cal3")
+ var btn4 = document.getElementById("cal4")
+ var btn5 = document.getElementById("cal5")
+ var btn6 = document.getElementById("cal6")
+ var btn7 = document.getElementById("cal7")
+ var btn8 = document.getElementById("cal8")
+ var btn9 = document.getElementById("cal9")
+ var btn10 = document.getElementById("cal10")
+ var btn11 = document.getElementById("cal11")
+ var btn12 = document.getElementById("cal12")
+ var btn13 = document.getElementById("cal13")
+ var btn14 = document.getElementById("cal14")
+ var btn15 = document.getElementById("cal15")
+ var btn16 = document.getElementById("result")
+
+function cal() {
+
+    
+
+}
